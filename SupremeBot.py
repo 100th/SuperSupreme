cadef0653d5a3c36f5bb6758e01cb4f2d66517ed
Jonghyun Lee <36814410+only-hub@users.noreply.github.com>
2018-06-09 18:11:33 +0900

    웹 테스트 완료
    웹 테스트 완료

--- SupremeBot.py
+++ SupremeBot.py
@@ -27,7 +27,18 @@ class QtDesigner(QMainWindow, form_class):
         self.timer.start(1000)
         self.timer.timeout.connect(self.timeout)
 
-        self.lineEdit.textChanged.connect(self.name_changed)
+        self.lineEdit.textChanged.connect(self.account_changed)
+        self.lineEdit_2.textChanged.connect(self.account_changed_2)
+        self.lineEdit_3.textChanged.connect(self.account_changed_3)
+        self.lineEdit_4.textChanged.connect(self.account_changed_4)
+        self.lineEdit_5.textChanged.connect(self.account_changed_5)
+        self.lineEdit_6.textChanged.connect(self.account_changed_6)
+        self.lineEdit_7.textChanged.connect(self.account_changed_7)
+        self.lineEdit_8.textChanged.connect(self.account_changed_8)
+        self.lineEdit_9.textChanged.connect(self.account_changed_9)
+        self.lineEdit_10.textChanged.connect(self.account_changed_10)
+        self.lineEdit_11.textChanged.connect(self.account_changed_11)
+        self.lineEdit_12.textChanged.connect(self.account_changed_12)
         self.pushButton.clicked.connect(self.check)
 
     # 현재 시간 표시
@@ -37,9 +48,30 @@ class QtDesigner(QMainWindow, form_class):
         time_msg = "Current Time : " + text_time
         self.statusbar.showMessage(time_msg)
 
-    def name_changed(self, name):
-        self.name = self.lineEdit.text()
-        print(name)
+    def account_changed(self, info):
+        self.info = self.lineEdit.text()
+    def account_changed_2(self, info_2):
+        self.info_2 = self.lineEdit_2.text()
+    def account_changed_3(self, info_3):
+        self.info_3 = self.lineEdit_3.text()
+    def account_changed_4(self, info_4):
+        self.info_4 = self.lineEdit_4.text()
+    def account_changed_5(self, info_5):
+        self.info_5 = self.lineEdit_5.text()
+    def account_changed_6(self, info_6):
+        self.info_6 = self.lineEdit_6.text()
+    def account_changed_7(self, info_7):
+        self.info_7 = self.lineEdit_7.text()
+    def account_changed_8(self, info_8):
+        self.info_8 = self.lineEdit_8.text()
+    def account_changed_9(self, info_9):
+        self.info_9 = self.lineEdit_9.text()
+    def account_changed_10(self, info_10):
+        self.info_10 = self.lineEdit_10.text()
+    def account_changed_11(self, info_11):
+        self.info_11 = self.lineEdit_11.text()
+    def account_changed_12(self, info_12):
+        self.info_12 = self.lineEdit_12.text()
 
     def check(self):
         #print(self.name)
@@ -47,12 +79,24 @@ class QtDesigner(QMainWindow, form_class):
         #print(myname)
 
         driver = webdriver.PhantomJS('/Users/paramount/Downloads/phantomjs/bin/phantomjs')
-        driver.get('https://www.google.co.kr/')
+        driver.get('https://okky.kr/user/register')
 
-        assert 'Google' in driver.title
+        assert 'OKKY' in driver.title
 
-        input_element = driver.find_element_by_name('q')
-        input_element.send_keys(self.name)
+        input_element = driver.find_element_by_name('username')
+        input_element.send_keys(self.info)
+
+        input_element = driver.find_element_by_name('password')
+        input_element.send_keys(self.info_2)
+
+        input_element = driver.find_element_by_name('person.email')
+        input_element.send_keys(self.info_3)
+
+        input_element = driver.find_element_by_name('person.fullName')
+        input_element.send_keys(self.info_4)
+
+        input_element = driver.find_element_by_name('avatar.nickname')
+        input_element.send_keys(self.info_5)
 
         driver.save_screenshot('search_results.png')
 
