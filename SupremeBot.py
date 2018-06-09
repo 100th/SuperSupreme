a09208248efff05f68b27142b39c302077f8cd43
Jonghyun Lee <36814410+only-hub@users.noreply.github.com>
2018-06-09 17:42:19 +0900

    타이핑 받은 문자를 출력 성공
    타이핑 받은 문자를 출력 성공

--- SupremeBot.py
+++ SupremeBot.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5 import uic
+import BotAccount
+from selenium.webdriver.common.keys import Keys
+
+# IP 우회 접속
 """
 service_args = [ '--proxy=localhost:9150', '--proxy-type=socks5', ]
 driver = webdriver.PhantomJS(executable_path='/Users/paramount/Downloads/phantomjs/bin/phantomjs', service_args=service_args)
@@ -11,6 +15,7 @@ driver.get('https://www.supremenewyork.com/shop')
 
 driver.save_screenshot('search_results.png')
 """
+
 form_class = uic.loadUiType("SuperSupreme.ui")[0]
 
 class QtDesigner(QMainWindow, form_class):
@@ -22,11 +27,35 @@ class QtDesigner(QMainWindow, form_class):
         self.timer.start(1000)
         self.timer.timeout.connect(self.timeout)
 
+        self.lineEdit.textChanged.connect(self.name_changed)
+        self.pushButton.clicked.connect(self.check)
+
+    # 현재 시간 표시
     def timeout(self):
         current_time = QTime.currentTime()
         text_time = current_time.toString("hh:mm:ss")
         time_msg = "Current Time : " + text_time
         self.statusbar.showMessage(time_msg)
+
+    def name_changed(self, name):
+        self.name = self.lineEdit.text()
+        print(name)
+
+    def check(self):
+        #print(self.name)
+        #myname = self.name
+        #print(myname)
+
+        driver = webdriver.PhantomJS('/Users/paramount/Downloads/phantomjs/bin/phantomjs')
+        driver.get('https://www.google.co.kr/')
+
+        assert 'Google' in driver.title
+
+        input_element = driver.find_element_by_name('q')
+        input_element.send_keys(self.name)
+
+        driver.save_screenshot('search_results.png')
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
